799f7c06aa9b342298fa5bd94e22a972651ca25c
Neville Jeremy <nevillejro.laia@gmail.com>
2025-08-20 22:59:29 +0700

    add lib insertion sort algorithm

---
+++ libs/insertionSort.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+void printArray(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+void insertionSort(int arr[], int n)
+{
+    for (int i = 1; i < n; i++)
+    {
+        int key = arr[i];
+        int j = i - 1;
+
+        while (j >= 0 && arr[j] > key)
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+
+        arr[j + 1] = key;
+    }
+}
+
+int main()
+{
+    int arr[] = {5, 4, 2, 1, 6, 3};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Array sebelum sorting\n");
+    printArray(arr, n);
+
+    insertionSort(arr, n);
+
+    printf("Array setelah sorting\n");
+    printArray(arr, n);
+
+    return 0;
+}
